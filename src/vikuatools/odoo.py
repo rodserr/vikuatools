5c52d14213da9fd248f35ff3e700c970ad660291
rodserr <rodrigoserranom8@gmail.com>
2022-03-25 13:36:41 +0000

    feat: add analytic account to move lines and clean currency model

--- src/vikuatools/odoo.py
+++ src/vikuatools/odoo.py
@@ -78,12 +78,13 @@ def clean_move_line(df):
       move_line_df = unlist_column(move_line_df, 'currency_id', ['currency_id','currency_description'])
       move_line_df = unlist_column(move_line_df, 'journal_id', ['journal_id','journal_description'])
       move_line_df = unlist_column(move_line_df, 'tax_fiscal_country_id', ['tax_fiscal_country_id','tax_fiscal_country_description'])
+      move_line_df = unlist_column(move_line_df, 'analytic_account_id', ['analytic_account_id','analytic_account_description'])
       
       # Extract just first tag_id
       move_line_df['analytic_tag_ids'] = move_line_df['analytic_tag_ids'].str[0]
       
       # Drop columns
-      move_line_df = move_line_df.drop(['account_description', 'tax_fiscal_country_id', 'partner_id'], axis = 'columns')
+      move_line_df = move_line_df.drop(['account_description', 'tax_fiscal_country_id', 'partner_id', 'analytic_account_id'], axis = 'columns')
       
       # Convert Datetimes
       move_line_df['date'] = move_line_df['date'].apply(pd.to_datetime)
@@ -101,7 +102,7 @@ def clean_move_line(df):
 def clean_account(df):
   
   """
-  Clean df, which contains data from move.line collection
+  Clean df, which contains data from account.account collection
   
   df: pd.df
   
@@ -128,7 +129,7 @@ def clean_account(df):
 def clean_analytic_tag(df):
   
   """
-  Clean df, which contains data from move.line collection
+  Clean df, which contains data from account.analytic.tag collection
   
   df: pd.df
   
@@ -144,6 +145,62 @@ def clean_analytic_tag(df):
   df=df.replace({False: None})
   
   return df
+
+def clean_analytic_account(df):
+  
+  """
+  Clean df, which contains data from account.analytic.account collection
+  
+  df: pd.df
+  
+  return: pd.df
+  """
+  
+  if df.empty:
+    return df
+  
+  df = df.rename(columns={"name": "analytic_account_name"})
+  
+  df['id'] = df['id'].apply(int_to_string)
+  df=df.replace({False: None})
+  
+  return df
+
+def clean_currency_rate(df):
+    
+    """
+    Clean df, which contains data from res.currency.rate collection
+  
+    df: pd.df
+  
+    return: pd.df
+    """
+    
+    if df.empty:
+      return df
+    
+    currency_df = df.copy()
+    
+    if not currency_df.empty:
+      # Split columns ids and descriptions
+      currency_df = unlist_column(currency_df, 'company_id', ['company_id','company_description'])
+      currency_df = unlist_column(currency_df, 'currency_id', ['currency_id','currency_description'])
+      
+      # Convert Datetimes
+      currency_df['date'] = currency_df['name'].apply(pd.to_datetime)
+      
+      # Drop columns
+      currency_df = currency_df.drop(['name'], axis = 'columns')
+      
+      # Convert Integer to string
+      int_to_str_columns = ['id', 'company_id', 'currency_id']
+      currency_df[int_to_str_columns] = currency_df[int_to_str_columns].applymap(int_to_string)
+      
+      # Replace False with None
+      currency_df = currency_df.replace({False: None})
+      
+    return currency_df
+
 
 def split_column(df: pd.DataFrame, column_to_split: str, sep = ' - ', prefix = 'category_'):
     
